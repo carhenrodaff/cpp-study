760e02536facf61453355eb94fd63ce43f89b3c4
Carlos <affonso.carlos98@gmail.com>
2023-05-19 19:25:47 -0400

    Fixed issue w/ flags

--- Chpt4Drill/main.cpp
+++ Chpt4Drill/main.cpp
@@ -10,31 +10,38 @@ int main() {
     bool nearEqual = false;
     char quit;
     while(true){
-        cin >> int1;
-        cin >> int2;
+        cin >> int1 >> int2;
         if (int1 > int2){
             larger = int1;
             smaller = int2;
         }
-        else if (int1 < int2){
+        if (int1 < int2){
             larger = int2;
             smaller = int1;
         }
-        else if (){
+        if (int1 == int2){
             equalFlag = true;
         }
-        if (!equalFlag) {
+        if (int1/int2 < 1.0/100.0){
+            nearEqual = true;
+        }
+        if (!equalFlag || !nearEqual) {
             cout << "The smaller value is: " << smaller << endl;
             cout << "The larger value is: " << larger << endl;
         }
-        else{
+        if(equalFlag){
             cout << "The numbers are equal." << endl;
+        }
+        if(nearEqual) {
+            cout << "These numbers are near equal" << endl;
         }
         cout << "Enter | to quit or any other character to continue\n";
         cin >> quit;
         if(quit == '|'){
             break;
         }
+        nearEqual = false;
+        equalFlag = false;
     }
     return 0;
 }
